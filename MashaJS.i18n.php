51b7d4c0ddce50e4f681765631e956a3771c153c
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-10-21 20:14:32 +0400

    Description using wiki markup.

--- MashaJS.i18n.php
+++ MashaJS.i18n.php
@@ -2,9 +2,9 @@
 
 $messages = array();
 $messages['en'] = array(
-  'mashajs-description' => 'This extension allows to use features of <a href="http://mashajs.com/">MaSha.JS</a> (Mark & Share) library.'
+  'mashajs-description' => 'This extension allows to use features of [http://mashajs.com MaSha.JS] (Mark & Share) library.'
 );
 $messages['ru'] = array(
-  'mashajs-description' => 'Это расширение позволяет использовать функциональность библиотеки <a href="http://mashajs.com/">MaSha.JS</a> (Mark & Share).'
+  'mashajs-description' => 'Это расширение позволяет использовать функциональность библиотеки [http://mashajs.com/ MaSha.JS] (Mark & Share).'
 );
 
